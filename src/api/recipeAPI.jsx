b613d3c20dafaad30ab8f5a79e6f2f73a3e65335
jchung7v <juanchung7v@gmail.com>
2024-09-22 00:48:56 -0700

    Updated API calls to use Netlify severless functions

--- src/api/recipeAPI.jsx
+++ src/api/recipeAPI.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5001/api";
+const BASE_URL = "/.netlify/functions";
 
 export const findRecipeByIngredients = async (ingredients) => {
   const params = {
@@ -11,7 +11,7 @@ export const findRecipeByIngredients = async (ingredients) => {
   try {
     const response = await axios.get(`${BASE_URL}/recipes`, {
       params,
-      timeout: 10000, // Set a 10-second timeout
+      timeout: 10000,
     });
     return response.data;
   } catch (error) {
@@ -23,6 +23,7 @@ export const findRecipeByIngredients = async (ingredients) => {
 export const findRecipeById = async (id) => {
   try {
     const response = await axios.get(`${BASE_URL}/recipes/${id}/information`, {
+      params: { id },
       timeout: 10000,
     });
     return response.data;
@@ -39,3 +40,45 @@ export const findRecipeById = async (id) => {
     }
   }
 };
+
+// import axios from "axios";
+
+// const BASE_URL = "http://localhost:5001/api";
+
+// export const findRecipeByIngredients = async (ingredients) => {
+//   const params = {
+//     ingredients: ingredients.join(","),
+//     number: 3,
+//   };
+
+//   try {
+//     const response = await axios.get(`${BASE_URL}/recipes`, {
+//       params,
+//       timeout: 10000,
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching recipes: ", error);
+//     throw error;
+//   }
+// };
+
+// export const findRecipeById = async (id) => {
+//   try {
+//     const response = await axios.get(`${BASE_URL}/recipes/${id}/information`, {
+//       timeout: 10000,
+//     });
+//     return response.data;
+//   } catch (error) {
+//     if (error.response) {
+//       console.error("Error fetching recipe:", error.response.data.error);
+//       throw new Error(error.response.data.error);
+//     } else if (error.request) {
+//       console.error("No response received from the server.");
+//       throw new Error("No response received from the server.");
+//     } else {
+//       console.error("Error:", error.message);
+//       throw new Error("An unexpected error occurred.");
+//     }
+//   }
+// };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,8 @@ import CurrentGroceryList from "./CurrentGroceryList.jsx";
 import AppHeader from "./AppHeader.jsx";
 import { AppState } from "../providers/AppState.jsx";
 import AllGroceryLists from "./AllGroceryLists.jsx";
-// import { useEffect } from "react";
-// import { initializeDatabase } from "../utils.js";
 
 function App() {
-  // useEffect(() => {
-  //   initializeDatabase();
-  // }, []);
   return (
     <AppState>
       <Box sx={{ display: "flex" }}>
