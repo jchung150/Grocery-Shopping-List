07b9e22662bbff70890ce2d5fabf558c0c990fe3
jchung7v <juanchung7v@gmail.com>
2024-09-22 00:58:41 -0700

    Fixed the BASE URL

--- src/api/recipeAPI.jsx
+++ src/api/recipeAPI.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "/.netlify/functions";
+const BASE_URL = "/api";
 
 export const findRecipeByIngredients = async (ingredients) => {
   const params = {
